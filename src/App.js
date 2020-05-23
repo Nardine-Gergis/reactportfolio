926a12f3a5b49f0ee00cc68ef5d1f7c856aad210
Nardne-Gergis <nardinegergis@gmail.com>
2020-05-23 11:18:24 -0400

    edited the navbar

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ class App extends React.Component {
         <Container className="p-0" fluid={true}>
           
           <Navbar className="border-bottom" bg="transparent" expand="lg">
-            <Navbar.Brand>Nardine George</Navbar.Brand>
+            <Navbar.Brand><p>Nardine George</p></Navbar.Brand>
 
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/about">About</Link>
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <Link className="nav-link" to="/"><p>Home</p></Link>
+                <Link className="nav-link" to="/about"><p>About</p></Link>
+                <Link className="nav-link" to="/contact"><p>Contact</p></Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
